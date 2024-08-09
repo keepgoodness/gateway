7638cef8b4a2ae0d654768d6a6ae2dd5cabab61a
keepgoodness <yordanlazarov83@gmail.com>
2024-08-09 14:46:24 +0300

    mysql-service/ no validation is needed because it is implemented in                gateway-service at UI level. Here is concrete communication.

--- mysql-service/src/main/java/com/example/db/handlers/GlobalExceptionHandler.java
+++ mysql-service/src/main/java/com/example/db/handlers/GlobalExceptionHandler.java
@@ -1,37 +1,15 @@
-package com.example.db.exception;
+package com.example.db.handlers;
 
+import com.example.db.exception.ResourceAlreadyExistsException;
 import com.example.db.model.response.ErrorDetails;
-import com.example.db.model.response.FixerResponse;
-import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
-    private final ModelMapper modelMapper;
-
-    public GlobalExceptionHandler(ModelMapper modelMapper) {
-        this.modelMapper = modelMapper;
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorDetails> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        StringBuilder message = new StringBuilder("Validation error: ");
-        ex.getBindingResult().getAllErrors().forEach(error ->
-                message.append(error.getDefaultMessage()).append("; ")
-        );
-        ErrorDetails errorDetails = new ErrorDetails();
-        errorDetails.setCode(HttpStatus.BAD_REQUEST.value());
-        errorDetails.setInfo(message.toString());
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(errorDetails);
-    }
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ErrorDetails> notCorrectJson(HttpMessageNotReadableException ex){
         ErrorDetails errorDetails = new ErrorDetails();
